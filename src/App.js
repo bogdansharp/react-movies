7d18f391a6dbe4c23c8230cf5d66b037bbc01134
Bogdan Bondarenko <bogdansharpy@gmail.com>
2024-01-12 01:49:26 +0000

    layout: Header Main Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Main from './layout/Main';
+import Footer from './layout/Footer';
+import Header from './layout/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App flex flex-col min-h-screen">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +20,10 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Header title='React Movies'/>
+      <Main/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/layout/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Main() {
+    return (
+        <main className="flex-grow">
+            <div className="container mx-auto text-center">
+
+            </div>
+        </main>
+    );
+}
